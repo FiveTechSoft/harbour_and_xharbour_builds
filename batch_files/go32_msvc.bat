a43248dfe878ed6b28ebd46059a955cef1032b4b
Enrico Maria Giordano <e.m.giordano@emagsoftware.it>
2023-10-23 10:29:07 +0200

    Update go32_msvc.bat

--- batch_files/go32_msvc.bat
+++ batch_files/go32_msvc.bat
@@ -4,12 +4,12 @@ SET HB_USER_CFLAGS=-MD -MP -O1 -DHAVE_SEARCH_H
 
 SET HB_INSTALL_PREFIX=%cd%\harbourmsc32
 
-set HB_WITH_OPENSSL=c:\OpenSSL-Win32\include
+SET HB_WITH_OPENSSL=c:\OpenSSL-Win32\include
 
-set HB_WITH_CURL=c:\curl32\include
-set HB_STATIC_CURL=yes
+SET HB_WITH_CURL=c:\curl32\include
+SET HB_STATIC_CURL=yes
 
-set HB_BUILD_DYN=no
-set HB_BUILD_CONTRIB_DYN=no
+SET HB_BUILD_DYN=no
+SET HB_BUILD_CONTRIB_DYN=no
 
 win-make.exe clean install
