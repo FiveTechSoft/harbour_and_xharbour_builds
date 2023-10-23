8b4da90f80133c941bdf44c86e17e0c4fed19ba2
Enrico Maria Giordano <e.m.giordano@emagsoftware.it>
2023-10-23 11:42:54 +0200

    Update go64_msvc.bat

--- batch_files/go64_msvc.bat
+++ batch_files/go64_msvc.bat
@@ -4,8 +4,6 @@ CALL "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\v
 
 SET HB_USER_CFLAGS=-MD -MP -O1 -DHAVE_SEARCH_H
 
-SET HB_INSTALL_PREFIX=%cd%\harbourmsc64
-
 SET HB_WITH_OPENSSL=c:\OpenSSL-Win64\include
 
 SET HB_WITH_CURL=c:\curl64\include
@@ -14,4 +12,4 @@ SET HB_STATIC_CURL=yes
 SET HB_BUILD_DYN=no
 SET HB_BUILD_CONTRIB_DYN=no
 
-win-make.exe clean install
+win-make.exe clean
