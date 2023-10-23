e9c88f70cf36357abdbd1b87ac48d0dc2e90a475
Enrico Maria Giordano <e.m.giordano@emagsoftware.it>
2023-10-23 10:29:44 +0200

    Update go64_msvc.bat

--- batch_files/go64_msvc.bat
+++ batch_files/go64_msvc.bat
@@ -1,15 +1,15 @@
-call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+CALL "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
-set HB_USER_CFLAGS=-MD -MP -O1 -DHAVE_SEARCH_H
+SET HB_USER_CFLAGS=-MD -MP -O1 -DHAVE_SEARCH_H
 
-set HB_INSTALL_PREFIX=%cd%\harbourmsc64
+SET HB_INSTALL_PREFIX=%cd%\harbourmsc64
 
-set HB_WITH_OPENSSL=c:\OpenSSL-Win64\include
+SET HB_WITH_OPENSSL=c:\OpenSSL-Win64\include
 
-set HB_WITH_CURL=c:\curl64\include
-set HB_STATIC_CURL=yes
+SET HB_WITH_CURL=c:\curl64\include
+SET HB_STATIC_CURL=yes
 
-set HB_BUILD_DYN=no
-set HB_BUILD_CONTRIB_DYN=no
+SET HB_BUILD_DYN=no
+SET HB_BUILD_CONTRIB_DYN=no
 
 win-make.exe clean install
