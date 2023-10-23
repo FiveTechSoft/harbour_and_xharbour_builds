cf70dc223cbd1698d0d82e21cfe65b94f3639b55
Enrico Maria Giordano <e.m.giordano@emagsoftware.it>
2023-10-23 21:29:41 +0200

    Update go32_msvc.bat

--- batch_files/go32_msvc.bat
+++ batch_files/go32_msvc.bat
@@ -2,6 +2,8 @@
 
 CALL "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 
+SET HB_INSTALL_PREFIX=%cd%\harbour_msvc32
+
 SET HB_USER_CFLAGS=-MD -MP -O1 -DHAVE_SEARCH_H
 
 SET HB_WITH_OPENSSL=c:\OpenSSL-Win32\include
@@ -12,4 +14,4 @@ SET HB_STATIC_CURL=yes
 SET HB_BUILD_DYN=no
 SET HB_BUILD_CONTRIB_DYN=no
 
-win-make.exe clean
+win-make clean install
