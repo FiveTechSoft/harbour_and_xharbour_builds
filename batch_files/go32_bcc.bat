8c35505950a0d62257d39122f87f23d7e685c183
Enrico Maria Giordano <e.m.giordano@emagsoftware.it>
2023-10-24 21:08:58 +0200

    Update go32_bcc.bat

--- batch_files/go32_bcc.bat
+++ batch_files/go32_bcc.bat
@@ -2,19 +2,17 @@
 
 SET PATH=c:\bcc7\bin
 
-SET HB_INSTALL_PREFIX=%cd%\harbour_bcc32
-
 SET HB_USER_CFLAGS=-w!-
 SET HB_USER_LDFLAGS=-ap
 
-SET HB_WITH_CURL=e:\xhbsvn\curl\include
+SET HB_WITH_CURL=c:\curl32\include
 SET HB_STATIC_CURL=yes
 
-SET HB_WITH_OPENSSL=e:\xhbsvn\openssl-win32\include
+SET HB_WITH_OPENSSL=c:\OpenSSL-Win32\include
 
 SET HB_BUILD_DYN=no
 SET HB_BUILD_CONTRIB_DYN=no
 
 IF EXIST lib\3rd\win\bcc REN lib\3rd\win\bcc bccx
 
-win-make clean install
+win-make clean
