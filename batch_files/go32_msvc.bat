dde431498442eaa6ba4601e4119fbf353520f9d0
FiveTech Software <antonio.fivetech@gmail.com>
2025-01-14 11:51:23 +0100

    Update go32_msvc.bat

--- batch_files/go32_msvc.bat
+++ batch_files/go32_msvc.bat
@@ -2,7 +2,7 @@
 
 CALL "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 
-SET HB_USER_CFLAGS=-MD -MP -O1 -DHAVE_SEARCH_H
+SET HB_USER_CFLAGS=-MD -MP -DHAVE_SEARCH_H
 
 SET HB_WITH_CURL=c:\curl32\include
 SET HB_STATIC_CURL=yes
